408d0f46091691648f377fba03d9cd8954ee2650
junghyun1009 <junghyunk1009@gmail.com>
2022-03-13 16:47:29 +0900

    update_0313

---
+++ self study/bfs.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def bfs(graph, start, visited):
+
+    queue = deque[start]
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력하기
+        v = queue.poleft()
+        print(v, end=' ')
+        # 아직 방문하지 않은 인접한 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+graph = [[], [2, 3, 8], [1, 7], [1, 4, 5], [3, 5], [3, 4], [7], [2, 6, 8], [1, 7]]
+
+vistied = [False] * 9
+bfs(graph, 1, visited)
