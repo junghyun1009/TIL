0c069dbe1226d36d0a826116b214ef89707cce40
junghyun1009 <junghyunk1009@gmail.com>
2022-04-08 22:33:18 +0900

    update_0408

--- SWEA/swea_2105/sol2.py
+++ SWEA/swea_2105/sol2.py
@@ -3,38 +3,70 @@ import sys
 sys.stdin = open('input.txt')
 
 
-def bfs(sr, sc):
-    global cnt
+def dfs(a):
+    if len(a) == 2:
+        rlt.append(a)
+        return
 
-    Q = []
-    Q.append([sr, sc])
+    for i in range(1, N-1):
+        dfs(a + [i])
+
+
+def bfs(sr, sc):
+    global maxcnt
+
+    # Q = []
+    # Q.append([sr, sc])
     dessert[cafe[sr][sc]] = 1
 
     # 오아 왼아 왼위 오위
     dr = [1, 1, -1, -1]
     dc = [1, -1, -1, 1]
 
-    while Q:
-        r, c = Q.pop(0)
+    # while Q:
+    #     r, c = Q.pop(0)
 
-        for a in range(1, N - 1):
-            for b in range(1, N - 1):
-                for dir in range(4):
-                    if dir % 2 == 0:
-                        for i in range(1, a+1):
-                            nr = r + dr[dir] * a
-                            nc = c + dc[dir] * a
+    for each in rlt:
+        w = each[0]
+        h = each[1]
+        flag = 1
 
-                            if 0 <= nr < N and 0 <= nc < N and dessert[cafe[nr][nc]] == 0:
-                                cnt += 1
-                            else:
-                                break
+        if sr + w + h < N and sc + w < N:
+            for dir in range(4):
+                if flag == 0:
+                    break
 
+                if dir % 2 == 0:
+                    for _ in range(w):
+                        nr = sr + dr[dir]
+                        nc = sc + dc[dir]
 
-                    else:
-                        nr = r + dr[dir] * b
-                        nc = c + dc[dir] * b
+                        if dessert[cafe[nr][nc]] == 0:
+                            dessert[cafe[nr][nc]] = 1
+                            sr = nr
+                            sc = nr
 
+                        else:
+                            flag = 0
+                            break
+
+                else:
+                    for _ in range(h):
+                        nr = sr + dr[dir]
+                        nc = sc + dc[dir]
+
+                        if dessert[cafe[nr][nc]] == 0:
+                            dessert[cafe[nr][nc]] = 1
+                            sr = nr
+                            sc = nr
+
+                        else:
+                            flag = 0
+                            break
+
+        rlt = 2 * (w + h)
+        if maxcnt < rlt:
+            maxcnt = rlt
 
 
 
@@ -45,6 +77,12 @@ for tc in range(1, T + 1):
     N = int(input())
     cafe = [list(map(int, input().split())) for _ in range(N)]
     dessert = [0] * 101
-    cnt = 0
+    maxcnt = 0
+
+    rlt = []
+    visited = [0] * (N - 1)
+    dfs([])
+    print(rlt)
+
     print(f'#{tc} ')
 
