51e05d892b58636280f87e148cfbadf467124e67
junghyun1009 <97649325+junghyun1009@users.noreply.github.com>
2022-06-12 22:04:41 +0900

    [Gold V] Title: 신기한 소수, Time: 1800 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -0,0 +1,26 @@
+def check(a):
+    for i in range(2, a//2):
+        if a % i == 0:
+            return False
+    return True
+
+def dfs(tmp):
+
+    if 1 <= len(tmp) <= N:
+        num = int(tmp)
+        if check(num) == False:
+            return
+
+        elif len(tmp) == N:
+            print(tmp)
+            return
+    
+    for i in can:
+        if len(tmp) == 0 and i != 1 and i != 9:    
+            dfs(tmp + str(i))
+        elif len(tmp) != 0 and i % 2 == 1 and i != 5:
+            dfs(tmp + str(i))
+
+N = int(input())
+can = [1, 2, 3, 5, 7, 9]
+dfs('')
