bc971f8f3834ab2b048b099f1b4f86422ffc7977
JungHyun Kim <97649325+junghyun1009@users.noreply.github.com>
2023-04-03 12:53:27 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,20 +1,26 @@
-n = int(input())
-m = int(input())
-graph = [[]*n for _ in range(n+1)]
-for _ in range(m):
-    a,b = map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-    
-cnt = 0
-visited = [0]*(n+1)
-def dfs(start):
-    global cnt
-    visited[start] = 1
-    for i in graph[start]:
-        if visited[i]==0:
-            dfs(i)
-            cnt +=1
- 
-dfs(1)
-print(cnt)
+from collections import deque
+
+N = int(input())
+P = int(input())
+pair = [list(map(int, input().split(' '))) for _ in range(P)]
+
+net = [[] for _ in range(N+1)]
+for i in pair:
+    net[i[0]].append(i[1])
+    net[i[1]].append(i[0])
+
+q = deque()
+visited = [0] * (N+1)
+visited[1] = 1
+for j in net[1]:
+    q.append(j)
+    visited[j] = 1
+
+while q:
+    cur = q.popleft()
+    for k in net[cur]:
+        if visited[k] == 0:
+            q.append(k)
+    visited[cur] = 1
+
+print(sum(visited)-1)
