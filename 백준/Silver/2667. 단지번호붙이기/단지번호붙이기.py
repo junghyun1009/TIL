ee2c6df832bfe3751aa571ab3deffbc15682d261
JungHyun Kim <97649325+junghyun1009@users.noreply.github.com>
2023-04-03 15:30:55 +0900

    [Silver I] Title: 단지번호붙이기, Time: 76 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,43 +1,39 @@
 from collections import deque
+            
+N = int(input())
+home = [list(map(int, input())) for _ in range(N)]
 
-n = int(input())
-a = [list(map(int, input())) for _ in range(n)]
-visited = [[False] * n for _ in range(n)]
+visited = [[0] * N for _ in range(N)]
+blocks = []
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(x, y):
-
-    queue = deque()
-    queue.append([x, y])
-    visited[x][y] = True
+def bfs(r, c):
+    q = deque()
+    q.append([r, c])
+    visited[r][c] = 1
     cnt = 1
 
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
 
-            if 0 <= nx < n and 0 <= ny < n:
-                if a[nx][ny] == 1 and not visited[nx][ny]:
-                    queue.append([nx, ny])
-                    visited[nx][ny] = True
-                    cnt += 1
+    while q:
+        cr, cc = q.popleft()
+        for d in range(4):
+            nr = cr + dr[d]
+            nc = cc + dc[d]
 
+            if 0 <= nr < N and 0 <= nc < N and home[nr][nc] == 1 and visited[nr][nc] == 0:
+                q.append([nr, nc])
+                visited[nr][nc] = 1
+                cnt += 1
     return cnt
 
-rlt = 0
-tmp = []
+for i in range(N):
+    for j in range(N):
+        if home[i][j] == 1 and visited[i][j] == 0:
+            ans = bfs(i, j)
+            blocks.append(ans)
 
-for i in range(n):
-    for j in range(n):
-        if a[i][j] == 1 and not visited[i][j]:
-            rlt += 1
-            tmp.append(bfs(i, j))
-
-tmp = sorted(tmp)
-print(rlt)
-for i in tmp:
-    print(i)
+blocks.sort()
+print(len(blocks))
+for k in blocks:
+    print(k)
